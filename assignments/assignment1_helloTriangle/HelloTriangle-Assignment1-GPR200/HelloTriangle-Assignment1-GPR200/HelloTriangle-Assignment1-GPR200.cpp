94c6344d2f4859baca5139f37c487d610d886dfe
NerdsyChamplain <joshua.scalia@mymail.champlain.edu>
2024-09-12 22:50:46 -0400

    Added setup
    Its now a cmake project

---
+++ assignments/assignment1_helloTriangle/HelloTriangle-Assignment1-GPR200/HelloTriangle-Assignment1-GPR200/HelloTriangle-Assignment1-GPR200.cpp
@@ -0,0 +1,12 @@
+﻿// HelloTriangle-Assignment1-GPR200.cpp : Defines the entry point for the application.
+//
+
+#include "HelloTriangle-Assignment1-GPR200.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
