3d756d0593bdfa2a8ddaecc559e19dfc6200b791
NerdsyChamplain <joshua.scalia@mymail.champlain.edu>
2024-09-16 01:05:56 -0400

    Finished assignment
    All goals are met (technically)

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -11,13 +11,22 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
-Shader ourShader("assets.vertexShader.vert", "assets.fragmentShader.frag");
-
+float posXBotRight = 0.5f;
+float posXBotLeft = -0.5f;
+float posXTop = 0.0f;
+float posYBotRight = -0.5f;
+float posYBotLeft = -0.5f;
+float posYTop = 0.5f;
+const float POSZ = 0.0f;
+float red = 1.0f;
+float green = 1.0f;
+float blue = 1.0f;
+const float NONTARGCOLORS = 0.0f;
 float vertices[] = {
-	// positions         // colors
-	 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-	-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-	 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+	// positions                           // colors
+	 posXBotRight, posYBotRight, POSZ,  red, NONTARGCOLORS, NONTARGCOLORS,   // bottom right
+	posXBotLeft, posYBotLeft, POSZ,  NONTARGCOLORS, green, NONTARGCOLORS,   // bottom left
+	 posXTop,  posYTop, POSZ,  NONTARGCOLORS, NONTARGCOLORS, blue    // top 
 };
 
 int main() {
@@ -36,6 +45,8 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
+	Shader ourShader("C:/Users/Josh/Documents/GitHub/gpr200-fa2024-joshua-scalia/assignments/assignment1_helloTriangle/assets/vertexShader.vert", "C:/Users/Josh/Documents/GitHub/gpr200-fa2024-joshua-scalia/assignments/assignment1_helloTriangle/assets/fragmentShader.frag");
+
 //shaders
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
@@ -44,57 +55,6 @@ int main() {
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
-	//vertex shader
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	//vertex shader compile error checking
-	int  success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-
-	//fragment shader
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	//fragment shader compile error checking
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-
-	//shader program object
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	//shader program compile checking
-	// glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-	//delete the shaders as theyre already linked in
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-	//copy vertex arrays
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	//set vertex pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
 	// position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -108,16 +68,48 @@ int main() {
 		//Clear framebuffer
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		float timeValue = glfwGetTime();
+		red = (sin(timeValue) / 2.0f) + 0.5f;
+		green = (cos(timeValue) / 2.0f) + 0.5f;
+		blue = (tan(timeValue) / 2.0f) + 0.5f;
+		posYBotRight += 0.0001f;
+		if (posYBotRight >= 0.6f)
+		{
+			posYBotRight = -0.5f;
+		}
+		posYBotLeft += 0.0001f;
+		if (posYBotLeft >= 0.6f)
+		{
+			posYBotLeft = -0.5f;
+		}
+		posYTop += 0.0001f;
+		if (posYTop >= 1.0f)
+		{
+			posYTop = 0.5f;
+		}
+		float vertices[] = {
+			// positions                           // colors
+			 posXBotRight, posYBotRight, POSZ,  red, NONTARGCOLORS, NONTARGCOLORS,   // bottom right
+			posXBotLeft, posYBotLeft, POSZ,  NONTARGCOLORS, green, NONTARGCOLORS,   // bottom left
+			 posXTop,  posYTop, POSZ,  NONTARGCOLORS, NONTARGCOLORS, blue    // top 
+		};
 		//Drawing happens here!
 		// update shader uniform
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUseProgram(shaderProgram);
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		ourShader.use();
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glBindVertexArray(VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		// position attribute
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		// color attribute
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
